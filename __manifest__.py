a7b1250086f1e6b515520ca45a9799ee0090a2ec
Orstein <esteban.yima@gmail.com>
2018-01-18 15:25:33 -0300

    2

---
+++ __manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    "name": """Global Discount\
+    """,
+    'version': '10.0.0.6.0',
+    'category': 'Account/invoice',
+    'sequence': 12,
+    'author':  'Daniel Santibáñez Polanco',
+    'website': 'https://globalresponse.cl',
+    'license': 'AGPL-3',
+    'summary': '',
+    'description': """
+Make available a global discount.
+===============================================================
+""",
+    'depends': [
+        'sale',
+        'l10n_cl_invoice',
+        'l10n_cl_dte',
+        ],
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/account_invoice.xml',
+        'views/layout.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
